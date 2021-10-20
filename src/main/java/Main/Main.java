22fb209882a9ee593b7c51a0ba74db47533bf0df
Alejandro <nelson.alejandrotorresg@gmail.com>
2021-10-20 09:38:45 +0200

    Initial commit

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -51,6 +51,8 @@ class Main {
 		// ejemploLeerDOM();
 		// ejemploEscribirXSTREAM();
 		// ejemploLeerXSTREAM();
+		
+		System.out.println("ddd");
 	}
 
 	private static void ejemploEscribirXSTREAM() {
