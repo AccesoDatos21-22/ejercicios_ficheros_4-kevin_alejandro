af2ddc7fe98ab56f107c4ffc019487b98ea20c46
Kevin61799 <71702377+Kevin61799@users.noreply.github.com>
2021-10-20 09:32:42 +0200

    Update MedicamentoAleatorio.java

--- src/main/java/dao/MedicamentoAleatorio.java
+++ src/main/java/dao/MedicamentoAleatorio.java
@@ -8,10 +8,35 @@ public class MedicamentoAleatorio implements MedicamentoDAO {
 	
 	public final static int TAM_NOMBRE = 30;
 	public final static int TAM_REGISTRO = 88;
+	public static final String FICHERO = "medicamentos.bin";
 	
 	@Override
 	public boolean guardar(Medicamento medicamento) {
-		
+		try (RandomAccessFile raf = new RandomAccessFile(FICHERO, "rw")) {
+			// posiciono en el registro del empleado a insertar
+			raf.seek(medicamento.getCod() * TAM_REGISTRO);
+			// Escribo el Código
+			raf.writeInt(medicamento.getCod());
+			// Escribo el nombre
+			StringBuilder nombre = new StringBuilder(medicamento.getNombre());
+			nombre.setLength(TAM_NOMBRE);
+			raf.writeChars(nombre.toString());
+			// Escribo el Precio
+			raf.writeDouble(medicamento.getPrecio());
+			// Escribo el Stock
+			raf.writeInt(medicamento.getStock());
+			// Escribo el StockMáximo
+			raf.writeInt(medicamento.getStockMaximo());
+			// Escribo el StockMínimo
+			raf.writeInt(medicamento.getStockMinimo());
+			// Escribo el Código del Proveedor
+			raf.writeInt(medicamento.getCodProveedor());
+		} catch (IOException exception) {
+			exception.printStackTrace();
+			return false;
+		}
+
+		System.out.println("Guardado");
 		return false;
 	}
 
